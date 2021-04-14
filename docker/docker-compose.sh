9f87e6d315812bb5f1bbac36b76ebe4e6e979130
Carrenolg <carrenolg@gmail.com>
2021-04-14 09:30:22 -0500

    first commit

---
+++ docker/docker-compose.sh
@@ -0,0 +1,5 @@
+# create and start containers (-d “detached” mode)
+$ docker-compose -f "file.yml" up -d
+
+# Build or rebuild services
+$ docker-compose -f "file.yml" build
---
+++ docker/docker.sh
@@ -0,0 +1,5 @@
+# get into container (-it interactive and terminal)
+$ docker exec -it 3239ecaf53ad /bin/bash
+
+# create resources
+$ docker volume create "name"
