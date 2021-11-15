bf42f98b116160763f0966a695704ed99da56629
Luis G. Carreno <carrenolg@gmail.com>
2021-11-15 12:23:07 -0500

    docker and mongodb

--- docker/docker-compose.sh
+++ docker/docker-compose.sh
@@ -3,3 +3,9 @@ $ docker-compose -f "file.yml" up -d
 
 # Build or rebuild services
 $ docker-compose -f "file.yml" build
+
+# Stop and delete containers
+$ docker-compose -f "file.yml" down
+
+# Delete all volumes
+$ docker volume rm $(docker volume ls -q)
